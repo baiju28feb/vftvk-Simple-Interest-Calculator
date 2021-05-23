685202b9feb4ae6143b2ba7a2f49e62fa701fae2
baiju <baiju28feb@gmail.com>
2021-05-23 10:11:23 +0530

    range and few other changes

--- script.js
+++ script.js
@@ -3,4 +3,6 @@ function compute()
     p = document.getElementById("principal").value;
     
 }
-        
+function updateTextInput(val) {
+    document.getElementById('rangeValue').value=val; 
+  }        
