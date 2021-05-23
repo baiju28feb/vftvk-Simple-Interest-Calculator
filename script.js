2152976cb333365046b20030726d279692cb061f
baiju <baiju28feb@gmail.com>
2021-05-23 17:28:38 +0530

    completed all functionality

--- script.js
+++ script.js
@@ -5,14 +5,24 @@ function compute()
    var t = document.getElementById("years").value;
    var si = 0;
    var d = new Date();
-   var maturityYear= d.getFullYear();
+   var currentYear= d.getFullYear();
+   var maturityYear;
+   if (p == null || p == undefined || p.length == 0) {
+    alert("Please enter a Amount \n");
+    return false;
+    }
+    if(parseInt(p)<0)
+    alert("Amount cannot be less than zero");
    if(p>0 && r> 0  && t>0)
    {
         si = (p*r*t)/100
-        maturityYear = d.getFullYear(maturityYear + t);
-   }
-   
+        maturityYear = parseInt(currentYear) + parseInt(t);
+        document.getElementById("resultpara").innerHTML = "If you Deposit "+p+"</Br> At an Interest rate of "+r+
+        " %.</Br> You will receive an amount of "+ si +",</Br> in the year " +maturityYear;
+    }  
+
 }
+   
 function updateTextInput(val) {
     var value = parseFloat(val).toFixed(2);
     document.getElementById('rangeValue').value=value; 
