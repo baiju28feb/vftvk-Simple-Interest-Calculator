54de9e1076eb20c4e6520682924e30a19c959934
baiju <baiju28feb@gmail.com>
2021-05-23 11:45:22 +0530

    design part

--- script.js
+++ script.js
@@ -1,8 +1,19 @@
 function compute()
 {
-    p = document.getElementById("principal").value;
-    
+   var p = document.getElementById("principal").value;
+   var r = document.getElementById("myRange").value;
+   var t = document.getElementById("years").value;
+   var si = 0;
+   var d = new Date();
+   var maturityYear= d.getFullYear();
+   if(p>0 && r> 0  && t>0)
+   {
+        si = (p*r*t)/100
+        maturityYear = d.getFullYear(maturityYear + t);
+   }
+   
 }
 function updateTextInput(val) {
-    document.getElementById('rangeValue').value=val; 
+    var value = parseFloat(val).toFixed(2);
+    document.getElementById('rangeValue').value=value; 
   }        
